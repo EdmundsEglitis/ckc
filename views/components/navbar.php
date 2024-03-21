75c490eb96d30a7f46e103c71899e81794a8fb02
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-21 13:47:04 +0200

    navbar

---
+++ views/components/navbar.php
@@ -0,0 +1,8 @@
+<header>
+  <nav>
+    <a href="/">pasakumi</a>
+    <a href="/add">pievienot pasakumu</a>
+    <a href="/delete">dzest pasakumu</a>
+    <a href="/change">rediget pasakumu</a>
+  </nav>
+</header>
--- views/index.view.php
+++ views/index.view.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Events</title>
-</head>
-<body>
+<?php require "components/head.php" ?>
+
+<?php require "components/navbar.php" ?>
     <h1>Pasākumi Cēsīs</h1>
     <ul>
     <?php foreach ($events as $event) { ?>
